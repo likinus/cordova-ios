dfba71d37d63ac22a270ec0268496c9de5fe57e5
Ihar Kazlouski <ia.kazlouski@vebtech.by>
2024-01-02 15:58:20 +0100

    init commit

---
+++ www/precache-manifest.e45a800b0e642cca30629c22b20b069c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbdda603d5bed383a31cf1f96588cf5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c83f29f0bf3d03d33232",
+    "url": "./static/css/2.e515a48f.chunk.css"
+  },
+  {
+    "revision": "e05341bfca018644937d",
+    "url": "./static/css/main.7eddda05.chunk.css"
+  },
+  {
+    "revision": "c83f29f0bf3d03d33232",
+    "url": "./static/js/2.b2a19944.chunk.js"
+  },
+  {
+    "revision": "e05341bfca018644937d",
+    "url": "./static/js/main.30a4ad12.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "cd20642c42b80435755cf5ffbc3044d3",
+    "url": "./static/media/attachment.cd20642c.svg"
+  },
+  {
+    "revision": "375a3cb4647261535f287bb27f758126",
+    "url": "./static/media/doc-file.375a3cb4.svg"
+  },
+  {
+    "revision": "d41480d0407dfec5cdff4f1d2caa2c1e",
+    "url": "./static/media/logout.d41480d0.svg"
+  }
+]);
